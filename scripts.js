a0df90bef19e294c0e67bb5f5080be082ef00368
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-11-23 17:51:41 +0300

    feat: add Accordion-2 section

--- scripts.js
+++ scripts.js
@@ -23,3 +23,32 @@ ACCORDION_1_QUESTIONs.forEach((q) => {
     }
   });
 });
+
+//_Accordion-2:
+const ACCORDION_2 = document.querySelector(".accordion-2");
+const ACCORDION_2_QUESTIONs = ACCORDION_2.querySelectorAll(
+  ".accordion-2__question"
+);
+
+ACCORDION_2_QUESTIONs.forEach((question) => {
+  question.addEventListener("click", (e) => {
+    const currentQuestion = e.currentTarget;
+    const currentItem = currentQuestion.parentNode;
+    const currentAnswer = currentQuestion.nextElementSibling;
+
+    ACCORDION_2.querySelectorAll(".accordion-2__item").forEach((item) => {
+      item.classList.remove("active");
+    });
+    ACCORDION_2.querySelectorAll(".accordion-2__answer").forEach((answer) => {
+      answer.style.maxHeight = 0;
+    });
+
+    if (!currentItem.classList.contains("active")) {
+      currentItem.classList.add("active");
+      currentAnswer.style.maxHeight = currentAnswer.scrollHeight + "px";
+    } else {
+      currentItem.classList.remove("active");
+      currentAnswer.style.maxHeight = 0;
+    }
+  });
+});
